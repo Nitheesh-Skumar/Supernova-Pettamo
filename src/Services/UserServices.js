f9cee58fb9668727198e2178c61bd118511663ad
Nitheeshskumar <nitheesh.s.k1710@gmail.com>
2022-06-22 00:15:40 +0530

    apiurl

--- src/Services/UserServices.js
+++ src/Services/UserServices.js
@@ -1,16 +1,17 @@
 import axios from 'axios'
+const apiUrl = ' https://pettamo.netlify.app'
 export const createUser = async (body) => {
-    const response = await axios.post("/.netlify/functions/createUser", body);
+    const response = await axios.post(apiUrl+"/.netlify/functions/createUser", body);
     return response.data
   };
 
  export const updateUser = async (body) => {
-    const response = await axios.post("/.netlify/functions/updateUser", body);
+    const response = await axios.post(apiUrl+"/.netlify/functions/updateUser", body);
     return response.data
   };
 
   export const authenticate = async (body) => {
-    const response = await axios.post("/.netlify/functions/authenticate", body);
+    const response = await axios.post(apiUrl+"/.netlify/functions/authenticate", body);
     return response.data
   };
 
@@ -18,36 +19,36 @@ export const createUser = async (body) => {
 
   //service
   export const listService = async (body) => {
-    const response = await axios.post("/.netlify/functions/listService", body);
+    const response = await axios.post(apiUrl+"/.netlify/functions/listService", body);
     return response.data
   };
 
   export const createService = async (body) => {
-    const response = await axios.post("/.netlify/functions/createService", body);
+    const response = await axios.post(apiUrl+"/.netlify/functions/createService", body);
     return response.data
   };
 
 //pets
 
 export const listPets = async (body) => {
-  const response = await axios.post("/.netlify/functions/listPets", body);
+  const response = await axios.post(apiUrl+"/.netlify/functions/listPets", body);
   return response.data
 };
 
 export const createPets = async (body) => {
-  const response = await axios.post("/.netlify/functions/createPet", body);
+  const response = await axios.post(apiUrl+"/.netlify/functions/createPet", body);
   return response.data
 };
 
 //appointments
 
 export const listAppointments = async (body) => {
-  const response = await axios.post("/.netlify/functions/listAppointments", body);
+  const response = await axios.post(apiUrl+"/.netlify/functions/listAppointments", body);
   return response.data
 };
 
 export const createAppointments = async (body) => {
-  const response = await axios.post("/.netlify/functions/createAppointment", body);
+  const response = await axios.post(apiUrl+"/.netlify/functions/createAppointment", body);
   return response.data
 };
 
