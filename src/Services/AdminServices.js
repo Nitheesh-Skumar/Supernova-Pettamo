4e4dbadd198a324d291ae674e8713cd5bea71162
Nitheeshskumar <nitheesh.s.k1710@gmail.com>
2022-06-21 23:46:13 +0530

    proxy settings

--- src/Services/AdminServices.js
+++ src/Services/AdminServices.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
-const apiUrl = ' https://pettamo.netlify.app'
 export const listUsers = async (todo) => {
-    const response = await axios.post(apiUrl+"/.netlify/functions/listUsers", todo);
+    const response = await axios.post("/.netlify/functions/listUsers", todo);
     return response.data
   };
 
   export const deleteUser = async (todo) => {
-    const response = await axios.post(apiUrl+"/.netlify/functions/deleteUser", todo);
+    const response = await axios.post("/.netlify/functions/deleteUser", todo);
     return response.data
   };
 
